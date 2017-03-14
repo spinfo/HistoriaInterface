489fef8443597496f15d65904c742739122be5c5
David Neugebauer <david.neugebauer@posteo.de>
2017-03-14 19:24:00 +0100

    Added tours update functionality.

--- models/tours.php
+++ models/tours.php
@@ -19,10 +19,11 @@ class Tours extends AbstractCollection {
         $this->join_coordinates_table = DB::table_name('tours_to_coordinates');
     }
 
-    // retrieves just the values from the tour's table with coordinate's and
-    // mapstop ids
-    // TODO: This is untested, may change soon anyway, test or remove!
-    public function get($id, $get_mapstop_ids = true, $get_coord_ids = true) {
+    /**
+     * Overrides AbstractCollection->get() to give the possibility of querying
+     * mapstop and coordinate ids.
+     */
+    public function get($id, $get_mapstop_ids = false, $get_coord_ids = false) {
         $sql = "SELECT * FROM $this->table WHERE id = %d";
         $result = DB::get_by_query($sql, array($id));
 
@@ -31,20 +32,15 @@ class Tours extends AbstractCollection {
             $sql = "SELECT id FROM " . Mapstops::instance()->table;
             $m_ids = DB::list($sql, array('tour_id' => $id));
             foreach($m_ids as $id_result) {
-                $result->mapstop_ids[] = intval($id_result->id);
+                $result->mapstop_ids[] = $id_result->id;
             }
         }
 
         if(!empty($result) && $get_coord_ids) {
-            $result->coordinate_ids = array();
-            $sql = "SELECT id FROM $this->join_coordinates_table";
-            $c_ids = DB::list($sql, array('tour_id' => $id));
-            foreach($c_ids as $id_result) {
-                $result->coordinate_ids[] = intval($id_result->id);
-            }
+            $result->coordinate_ids = $this->db_get_coordinate_ids($id);
         }
 
-        return $result;
+        return $this->instance_from_array($result);
     }
 
 
@@ -58,38 +54,118 @@ class Tours extends AbstractCollection {
         $values = $this->db_values($tour);
 
         DB::start_transaction();
-        $result_id = DB::insert($this->table, $values);
+        $tour_id = DB::insert($this->table, $values);
 
-        // insert tour's track with join relation
-        if($result_id != DB::BAD_ID && !empty($tour->coordinates)) {
-            $result_coords = $this->db_insert_coordinates($tour, $result_id);
+        // insert tour's track (coordinates) with join relation
+        $tour->coordinate_ids = array();
+        if($tour_id != DB::BAD_ID && !empty($tour->coordinates)) {
+            foreach($tour->coordinates as $coordinate) {
+                $result = Coordinates::instance()->save($coordinate);
+                if(empty($result)) {
+                    debug_log("Error saving coordinate for tour.");
+                    $this->rollback_insert($tour);
+                    return DB::BAD_ID;
+                } else {
+                    $tour->coordinate_ids[] = $result->id;
+                }
+            }
+            $result_coords = $this->db_insert_coordinate_joins($tour, $tour_id);
         }
 
         // check for errors and abort if there are any
-        if($result_id == DB::BAD_ID || !$result_coords) {
+        if($tour_id == DB::BAD_ID || !$result_coords) {
             debug_log("Errors while saving tour. Rolling back.");
-            $tour->id = DB::BAD_ID;
-            foreach($tour->coordinates as $coord) {
-                $coord->id = DB::BAD_ID;
-            }
-            DB::rollback_transaction();
+            $this->rollback_insert($tour);
             return DB::BAD_ID;
         }
 
         // at this point we may commit the results and return
         DB::commit_transaction();
-        $tour->id = $result_id;
+        $tour->id = $tour_id;
         return $tour->id;
     }
 
-    protected function db_update($tour) {}
+    protected function db_update($tour) {
+        // check if the tour is valid and abort if it isn't
+        if(!$tour->is_valid()) {
+            debug_log("Not updating invalid tour.");
+            return false;
+        }
+
+        DB::start_transaction();
+
+        // save all linked coordinate ids to restore them later if neccessary
+        $old_coordinate_ids = $tour->coordinate_ids;
+        $old_coordinates = $tour->coordinates;
+
+        // update linked coordinates
+        $coords_update_ok = $this->db_update_coordinates($tour);
+        if($coords_update_ok) {
+            // update the tour itself
+            $values = $this->db_values($tour);
+            $tour_update_ok = DB::update($this->table, $tour->id, $values);
+        }
+
+        if($coords_update_ok && $tour_update_ok) {
+            // commit the transaction, update coordinate ids and return
+            DB::commit_transaction();
+            return true;
+        } else {
+            // reset old id values, rollback and return false
+            $tour->coordinate_ids = $old_coordinate_ids;
+            $tour->coordinates = $old_coordinates;
+            debug_log("Tour update failed. Rolling back.");
+            DB::rollback_transaction();
+            return false;
+        }
+    }
 
     protected function db_delete($tour) {}
 
+    protected function instance_from_array($array) {
+        $array = (object) $array;
+
+        $tour = new Tour();
+
+        $this->update_values($tour, $array);
+        $this->set_abstract_model_values($tour, $array);
+
+        return $tour;
+    }
+
+    public function update_values($tour, $array) {
+        $array = (object) $array;
+
+        $tour->area_id = intval($array->area_id);
+        $tour->user_id = intval($array->user_id);
+        $tour->name = strval($array->name);
+        $tour->intro = strval($array->intro);
+        $tour->type = strval($array->type);
+        $tour->walk_length = intval($array->walk_length);
+        $tour->duration = intval($array->duration);
+        $tour->tag_what = strval($array->tag_what);
+        $tour->tag_where = strval($array->tag_where);
+        $tour->tag_when_start = floatval($array->tag_when_start);
+        // end date is either a float, when explicitly given or null
+        if(!is_null($array->tag_when_end)) {
+            $tour->tag_when_end = floatval($array->tag_when_end);
+        } else {
+            $tour->tag_when_end = null;
+        }
+        $tour->accessibility = strval($array->accessibility);
+
+        if(!is_null($array->coordinate_ids)) {
+            $tour->coordinate_ids = array_map('intval', $array->coordinate_ids);
+        }
+        if(!is_null($array->mapstop_ids)) {
+            $tour->mapstop_ids = array_map('intval', $array->mapstop_ids);
+        }
+    }
+
     private function db_values($tour) {
         $values = array(
-            'area_id' => strval($tour->area_id),
-            'user_id' => strval($tour->user_id),
+            'area_id' => intval($tour->area_id),
+            'user_id' => intval($tour->user_id),
             'name' => strval($tour->name),
             'intro' => strval($tour->intro),
             'type' => strval($tour->type),
@@ -102,31 +178,122 @@ class Tours extends AbstractCollection {
         );
         if(!is_null($tour->tag_when_end)) {
             $values['tag_when_end'] = floatval($tour->tag_when_end);
+        } else {
+            $values['tag_when_end'] = null;
         }
         return $values;
     }
 
-    // insert the tour's coordinates with join relation, return true on success
-    // and false on the first error (do as little as possible)
-    private function db_insert_coordinates($tour, $tour_id) {
-        foreach($tour->coordinates as $coord) {
-            try {
-                $coord_id = Coordinates::instance()->insert($coord);
-                if($coord_id != DB::BAD_ID) {
-                    $values =
-                        array( 'coordinate_id' => $coord_id, 'tour_id' => $tour_id);
-                    $join_id = DB::insert($this->join_coordinates_table, $values);
-                    if($join_id == DB::BAD_ID) {
-                        return false;
-                    }
-                } else {
-                    return false;
+    // retrieve ids of all coordinates joined to the tour in the database
+    private function db_get_coordinate_ids($tour_id) {
+        $result = array();
+        $sql = "SELECT coordinate_id FROM $this->join_coordinates_table";
+        $id_results = DB::list($sql, array('tour_id' => $tour_id));
+        foreach($id_results as $id_result) {
+            $result[] = intval($id_result->coordinate_id);
+        }
+        return $result;
+    }
+
+    // Update the tour's coordinates, return fals on any error else true.
+    // This will alter the $tour->coordinates and $tour->coordinate_ids fields
+    // if successful.
+    // The caller should make sure that this happens within a transaction.
+    private function db_update_coordinates($tour) {
+        $new_coordinate_ids = array();
+        $new_coordinates = array();
+        // update all coordinates and update the tour's coordinate_ids
+        foreach($tour->coordinates as $coordinate) {
+            $result = Coordinates::instance()->save($coordinate);
+            if(is_null($result)) {
+                debug_log("Error saving coordinate for tour update.");
+                return false;
+            }
+            $new_coordinate_ids[] = $coordinate->id;
+            $new_coordinates[] = $result;
+        }
+
+        // get all ids of coordinates linked to this tour
+        $old_coordinate_ids = $this->db_get_coordinate_ids($tour->id);
+
+        // determine the coordinates that have to be deleted and delete
+        $to_delete = array_diff($old_coordinate_ids, $new_coordinate_ids);
+        try {
+            foreach ($to_delete as $coord_id) {
+                $coord = Coordinates::instance()->get($coord_id);
+                // delete the coordinate
+                $res_coord = Coordinates::instance()->delete($coord);
+                // delete the join relation
+                $where = array(
+                    'tour_id' => $tour->id,
+                    'coordinate_id' => $coord_id
+                );
+                $res_join = DB::delete($this->join_coordinates_table, $where);
+                // check for errors
+                if(empty($res_coord) || $res_join != 1) {
+                    throw new DB_Exception("Error deleting coord or join row.");
                 }
-            } catch(DB_Exception $e) {
+            }
+        } catch(DB_Exception $e) {
+            debug_log("Error updating tour's coordinates: " . $e->getMessage());
+            return false;
+        }
+
+        // determine the coordinate ids that need new joins created
+        $to_insert = array_diff($new_coordinate_ids, $old_coordinate_ids);
+        foreach($to_insert as $coord_id) {
+            $join_id = $this->db_insert_single_coordinate_join($tour->id,
+                $coord_id);
+            if($join_id == DB::BAD_ID) {
+                debug_log("Error creating tour to coordinate join.");
+                return false;
+            }
+        }
+
+        // everything went okay, so set the new coordinate values on the tour
+        // object and return
+        $tour->coordinates = $new_coordinates;
+        $tour->coordinate_ids = $new_coordinate_ids;
+        return true;
+    }
+
+    // Insert a single coordinate join. Return the join's id or DB::BAD_ID on
+    // any error.
+    private function db_insert_single_coordinate_join($tour_id, $coord_id) {
+        try {
+            $values = array(
+                'coordinate_id' => $coord_id, 'tour_id' => $tour_id);
+            $join_id = DB::insert($this->join_coordinates_table, $values);
+            return $join_id;
+        } catch(DB_Exception $e) {
+            debug_log('Error creating tour to coord join: ' . $e->getMessage());
+            return DB::BAD_ID;
+        }
+    }
+
+    // Insert the tours to coordinates join relations, return true on success
+    // and false on the first error (do as little as possible).
+    // The caller should make sure that this happens within a transaction.
+    private function db_insert_coordinate_joins($tour, $tour_id) {
+        // iterate over coordinates
+        foreach($tour->coordinates as $coord) {
+            $join_id = $this->db_insert_single_coordinate_join($tour_id,
+                $coord->id);
+            if($join_id == DB::BAD_ID) {
                 return false;
             }
         }
         return true;
+    }
+
+    // reset id values created and rollback the current database transaction
+    private function rollback_insert($tour) {
+        $tour->id = DB::BAD_ID;
+        $tour->coordinate_ids = array();
+        foreach($tour->coordinates as $coord) {
+            $coord->id = DB::BAD_ID;
+        }
+        DB::rollback_transaction();
     }
 
 }
--- testing/tours_test.php
+++ testing/tours_test.php
@@ -2,6 +2,7 @@
 namespace SmartHistoryTourManager;
 
 require_once(dirname(__FILE__) . '/../models/areas.php');
+require_once(dirname(__FILE__) . '/../models/mapstops.php');
 require_once(dirname(__FILE__) . '/../models/tour.php');
 require_once(dirname(__FILE__) . '/../models/tours.php');
 
@@ -27,7 +28,7 @@ class ToursTest extends TestCase {
     }
 
     public function test_bad_create() {
-        // make tour invalidate the last coordinate
+        // make tour and invalidate by invalidating the last coordinate
         $tour = $this->helper->make_tour();
         $tour->coordinates[count($tour->coordinates) - 1]->lat = null;
 
@@ -52,11 +53,76 @@ class ToursTest extends TestCase {
         }
     }
 
+    public function test_get() {
+        $tour = $this->tours[0];
+
+        // make mapstops for the tour, the ids of which we can retrieve
+        $tour->mapstop_ids = array();
+        $mapstop = $this->helper->make_mapstop();
+        $mapstop->tour_id = $tour->id;
+        Mapstops::instance()->insert($mapstop);
+        $tour->mapstop_ids[] = $mapstop->id;
+
+        $from_db = Tours::instance()->get($tour->id, true, true);
+
+        // test general model validity
+        $this->assert_valid_model($from_db, $this->table, "get tour");
+
+        // test that the returned values match
+        $this->check_tour_values($from_db, $tour, "get tour");
+
+        // clean up the the mapstop and the posts created for it, reset vars
+        Mapstops::instance()->delete($mapstop);
+        $tour->mapstop_ids = null;
+        $this->helper->delete_wp_posts_created();
+    }
+
+    public function test_update() {
+        $tour = $this->tours[0];
+        $other = $this->helper->make_tour();
+
+        // change tour's values to other's values and check that it worked
+        Tours::instance()->update_values($tour, $other);
+        $this->check_tour_values($tour, $other, "value update on tour");
+
+        // change the tour's coordinates by removing one coordinate and adding
+        // one of those of the other's (not persisted, so without id)
+        $coord_removed = array_pop($tour->coordinates);
+        $id_removed = array_pop($tour->coordinate_ids);
+        array_push($tour->coordinates, array_pop($other->coordinates));
+
+        // save highest coordinate id before update to assert insert
+        $c_id_before = $this->helper->db_highest_id($this->coords_table);
+
+        // update and check the return value
+        $result = Tours::instance()->update($tour);
+        $this->assert($result != false,
+            "Should not return false on normal tour update");
+
+        // test values on tour retrieved from the database
+        $from_db = Tours::instance()->get($tour->id, true, true);
+        $this->check_tour_values($from_db, $tour, "normal tour update");
+
+        // test that old coord ids are no longer valid
+        $this->assert(!DB::valid_id($this->coords_table, $coord_removed->id),
+            "Tour update should have removed old coordinate.");
+
+        // test that the new coordinate has an id and exists in the database
+        $new_id = $from_db->coordinate_ids[count($tour->coordinate_ids) - 1];
+        $c_id_after = $this->helper->db_highest_id($this->coords_table);
+        $this->assert($c_id_after > $c_id_before,
+            "Tour update should have created coordinate.");
+        $this->assert($new_id == $c_id_after,
+            "New coordinate should have the right id on tour update.");
+    }
+
     public function do_test() {
         $this->setup();
 
         $this->test_create();
         $this->test_bad_create();
+        $this->test_get();
+        $this->test_update();
     }
 
     private function setup() {
@@ -65,6 +131,32 @@ class ToursTest extends TestCase {
         $this->join_table = Tours::instance()->join_coordinates_table;
         $this->coords_table = Coordinates::instance()->table;
         $this->tours = array();
+    }
+
+    private function check_tour_values($got, $expected, $test_name) {
+        $this->assert($got->user_id == $expected->user_id, "user id should match ($test_name).");
+        $this->assert($got->area_id == $expected->area_id, "area id should match ($test_name).");
+        $this->assert($got->name == $expected->name, "name should match ($test_name).");
+        $this->assert($got->intro == $expected->intro, "intro should match ($test_name).");
+        $this->assert($got->type == $expected->type, "type should match ($test_name).");
+        $this->assert($got->walk_length == $expected->walk_length, "walk_length should match ($test_name).");
+        $this->assert($got->duration == $expected->duration, "duration should match ($test_name).");
+        $this->assert($got->tag_what == $expected->tag_what, "tag_what should match ($test_name).");
+        $this->assert($got->tag_where == $expected->tag_where, "tag_where should match ($test_name).");
+        $this->assert($got->tag_when_start == $expected->tag_when_start, "tag_when_start should match ($test_name).");
+        $this->assert($got->tag_when_end == $expected->tag_when_end, "tag_when_end should match ($test_name).");
+        $this->assert($got->accessibility == $expected->accessibility, "accessibility should match ($test_name).");
+
+        if(!empty($expected->coordinate_ids)) {
+            // test for array equality (not identity) is sufficient
+            $this->assert($got->coordinate_ids == $expected->coordinate_ids,
+                "coordinate_ids should match ($test_name).");
+        }
+        if(!empty($expected->mapstop_ids)) {
+            // test for array equality (not identity) is sufficient
+            $this->assert($got->mapstop_ids == $expected->mapstop_ids,
+                "mapstop_ids should match ($test_name).");
+        }
     }
 
 }
