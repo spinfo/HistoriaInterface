82b9909af9f8253b3627d8493f79389c6b10edf3
David Neugebauer <david.neugebauer@posteo.de>
2017-03-17 12:52:36 +0100

    Allow test runner to log database queries.

--- test/run_tests.php
+++ test/run_tests.php
@@ -8,12 +8,28 @@ namespace SmartHistoryTourManager;
  *  ARG: 'api'          all api tests
  *  ARG: 'test-name'    the test by that name, e.g. 'places' or 'place_api'
  *
+ *  ARG: --queries      Debug log all database queries.
+ *
  * EXAMPLE: Running
  *
  *  'php run_tests.php unit area_api'
  *
  * will run all unit tests, followed by the area api test.
  */
+// PREPARE THE TEST ENVIRONMENT
+// redirect all debug logging to stdout
+require_once(dirname(__FILE__) . '/../logging.php');
+Logging::set_output(Logging::TO_STDOUT);
+
+// Make the test helper and load the wordpress envrionment, so that tests and
+// tested functions are able to call the wordpress api (especially the wpdb
+// database connection).
+require_once(dirname(__FILE__) . '/test_helper.php');
+$helper = new TestHelper();
+require_once($helper->config->wp_load_script);
+// test case is needed by all tests
+require_once(dirname(__FILE__) . '/test_case.php');
+
 
 // PARSE ARGUMENTS
 const UNIT_TESTS = array('places', 'areas', 'mapstops', 'tours');
@@ -33,6 +49,8 @@ if(empty($argv)) {
             $to_run = array_merge($to_run, API_TESTS);
         } else if(in_array($arg, UNIT_TESTS) || in_array($arg, API_TESTS)) {
             array_push($to_run, $arg);
+        } else if ($arg == '--queries') {
+            \add_filter('query', 'SmartHistoryTourManager\debug_log_query');
         } else {
             echo "ERROR: unknown test: $arg" . PHP_EOL;
             exit(-1);
@@ -40,21 +58,6 @@ if(empty($argv)) {
     }
 }
 $to_run = array_unique($to_run);
-
-
-// PREPARE THE TEST ENVIRONMENT
-// redirect all debug logging to stdout
-require_once(dirname(__FILE__) . '/../logging.php');
-Logging::set_output(Logging::TO_STDOUT);
-
-// Make the test helper and load the wordpress envrionment, so that tests and
-// tested functions are able to call the wordpress api (especially the wpdb
-// database connection).
-require_once(dirname(__FILE__) . '/test_helper.php');
-$helper = new TestHelper();
-require_once($helper->config->wp_load_script);
-
-require_once(dirname(__FILE__) . '/test_case.php');
 
 // RUN THE TESTS
 $unit_test_dir = dirname(__FILE__) . '/unit_tests';
