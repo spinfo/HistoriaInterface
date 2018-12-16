f4c99dfd95633f60b0ea5ff71f427814cdfa026a
David Schunke <david@schunke.me>
2018-12-17 00:56:33 +0100

    added migration2.sql

--- migration2.sql
+++ migration2.sql
@@ -1,2 +1,10 @@
+CREATE TABLE `wp_shtm_mapstops_to_scenes_copy` LIKE `wp_shtm_mapstops_to_scenes`;
+INSERT `wp_shtm_mapstops_to_scenes_copy` SELECT * FROM `wp_shtm_mapstops_to_scenes`;
+
+CREATE TABLE `wp_shtm_scenes_copy` LIKE `wp_shtm_scenes`;
+INSERT `wp_shtm_scenes_copy` SELECT * FROM `wp_shtm_scenes`;
 
 ALTER TABLE `wp_shtm_mapstops_to_scenes` MODIFY `scene_id` bigint(20) unsigned NOT NULL;
+
+ALTER TABLE `wp_shtm_scenes` DROP COLUMN `id`;
+ALTER TABLE `wp_shtm_scenes` CHANGE `post_id` `id` bigint(20) unsigned PRIMARY KEY NOT NULL;
